d0fddda58ac3fadc1cb92fb9d7f88824c379cd1e
anderskambenders <a.zetsmanis@mail.ru>
2025-05-09 10:06:07 +0300

    feat: add mocking node api

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -18,22 +18,22 @@ describe('resolveValue', () => {
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     const errorMessage = 'Error message';
-    expect(() => throwError(errorMessage)).toThrowError(errorMessage);
+    expect(() => throwError(errorMessage)).toThrow(errorMessage);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    expect(() => throwError()).toThrowError('Oops!');
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    expect(() => throwCustomError()).toThrowError(MyAwesomeError);
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    await expect(rejectCustomError()).rejects.toThrowError(MyAwesomeError);
+    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
